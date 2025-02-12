1d471aa304fb68fa1d2c15a5bff65c0fa276cb84
Mohammed Khalifah <tu4205521@taibahu.edu.sa>
2025-02-12 12:20:59 +0300

    تشغيل الفيديو قبل الصوت

--- Monitor.py
+++ Monitor.py
@@ -2,10 +2,11 @@ import subprocess
 import sys
 
 if __name__ == "__main__":
-    # تشغيل الملف SoundMonitor.py
-    process_audio = subprocess.Popen([sys.executable, "SoundMonitor.py"])
+
     # تشغيل الملف FaceMonitor.py
     process_project = subprocess.Popen([sys.executable, "FaceMonitor.py"])
+    # تشغيل الملف SoundMonitor.py
+    process_audio = subprocess.Popen([sys.executable, "SoundMonitor.py"])
 
     # الانتظار حتى تنتهي كلتا العمليتين (اختياري)
     process_audio.wait()
